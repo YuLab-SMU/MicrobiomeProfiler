b5dc78672f064f27e229cc6265c310d3625dc9d6
xiangpin <xshuangbin@163.com>
2023-04-20 13:53:58 +0800

    add enrichModule

---
+++ R/enrichModule.R
@@ -0,0 +1,54 @@
+#' Module enrichment for microbiome data
+#'
+#' @param gene a vector of K gene id (e.g. K00001).
+#' @param pvalueCutoff adjusted pvalue cutoff on enrichment tests to report.
+#' @param pAdjustMethod one of "holm","hochberg","hommel","bonferroni","BH",
+#' "BY","fdr","none".
+#' @param universe universe background genes. If missing, use all K genes.
+#' @param minGSSize minimal size of genes annotated by KEGG term for testing.
+#' @param maxGSSize maximal size of genes annotated for testing.
+#' @param qvalueCutoff qvalue cutoff on enrichment tests to report.
+#' @importFrom clusterProfiler enricher
+#' @importFrom methods slot<-
+#' @return A \code{enrichResult} instance.
+#' @export
+#' @examples
+#'
+#'   data(Rat_data)
+#'   ko <- enrichModule(Rat_data)
+#'   head(ko)
+#'
+enrichModule <- function(gene,
+                     pvalueCutoff      = 0.05,
+                     pAdjustMethod     = "BH",
+                     universe,
+                     minGSSize         = 10,
+                     maxGSSize         = 500,
+                     qvalueCutoff      = 0.2) {
+
+    if (all(grepl("^K", gene))){
+        use.gson <- module_gson.KO
+    }else{
+        use.gson <- module_gson.ec
+    } 
+
+    res <- enricher(gene,
+                    gson = use.gson,
+                    pvalueCutoff  = pvalueCutoff,
+                    pAdjustMethod = pAdjustMethod,
+                    universe      = universe,
+                    minGSSize     = minGSSize,
+                    maxGSSize     = maxGSSize,
+                    qvalueCutoff  = qvalueCutoff)
+    if (is.null(res))
+        return(res)
+
+    slot(res,"ontology") <- "KEGG Module"
+    slot(res,"organism") <- "microbiome"
+
+    return(res)
+}
+
+
+
+
